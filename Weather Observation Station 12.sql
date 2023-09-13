0bf306a2a9ff9a28be70dd8e826c84a484e5d6c5
Mustafa Cali <mustafacali98@gmail.com>
2023-09-13 10:46:49 +0300

    Update Weather Observation Station 12.sql

--- Weather Observation Station 12.sql
+++ Weather Observation Station 12.sql
@@ -1,3 +1,7 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+*/
+
 SELECT CITY FROM STATION
 WHERE CITY NOT LIKE '[aeiou]%' and CITY NOT LIKE '%[aeiou]'
 GROUP BY CITY;
